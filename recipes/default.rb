b7a3d1762803316b3ca72d8707b1739212268327
Cameron Johnston <cameron@needle.com>
2013-03-18 09:43:05 -0600

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: exception-test
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "cause an exception" do
+  command "I like to eat apples and bananas"
+end
